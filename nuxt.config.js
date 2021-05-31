fe63fb3e38c2397ab4824a4582cc269a918ab781
Erne <ernesto92.mg@gmail.com>
2021-05-31 17:47:49 -0400

    launching in 3,2,1...🚀

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  router: {
+    base: '/movies-demo/',
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Peliculas Demo',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Este es un demo que lista y permite buscar películas obtenidas de la api http://www.omdbapi.com/',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@mdi/font/css/materialdesignicons.min.css', '~/assets/main'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: ['~/components/UI'],
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
